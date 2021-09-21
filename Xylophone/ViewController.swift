e8ccb8225345ff9f5b771534600bf1475e594c6d
Tiago Teodoro <tiagoteo2001@gmail.com>
2021-09-21 15:59:32 +0100

    Full project commit.

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -6,16 +6,34 @@
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
+
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound(title: sender.currentTitle!)
+        
+        sender.alpha = 0.5
 
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+              sender.alpha = 1.0
+          }
+    }
     
-
+    func playSound(title: String) {
+        let url = Bundle.main.url(forResource: title, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
+    
 }
+
 
